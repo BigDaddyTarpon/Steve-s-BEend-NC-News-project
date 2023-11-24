4e38d16e99dec249976591b15640cbe4082846cf
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2023-11-24 11:25:05 +0000

    removed playground files

--- playground.sql
+++
@@ -1,8 +0,0 @@
-\c nc_news_test
--- example select * from articles
-
-`INSERT INTO comments(body, author, article_id) 
-      VALUES ($1, $2, $3) 
-      RETURNING *
-      ;`,
-      [body, username, article_id]
